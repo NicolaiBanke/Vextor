94a4270dd85db7886d9c5b62ff84de04c599f474
Nicolai Banke <nicolaibanke@hotmail.com>
2025-10-04 15:55:31 +0200

    refactor: __init__ method for Baxter class

--- Baxter.py
+++ Baxter.py
@@ -1,3 +1,33 @@
-class Baxter:
+import pandas as pd
+
+class Baxter(object):
+    def __init__(
+        self,
+        pnls=[],
+        unrlzd=[],
+        pnls_pct=[],
+        unrlzd_pct=[],
+        trade_dates=[],
+        dates=[],
+        entry=None,
+        inpos=0,
+        COMMS=0.05,
+    ) -> None:
+        self._pnls = pnls
+        self._unrlzd = unrlzd
+        self._pnls_pct = pnls_pct
+        self._unrlzd_pct = unrlzd_pct
+        self._trade_dates = trade_dates
+        self._dates = dates
+        self._entry = entry
+        self._inpos = inpos
+        self._COMMS = COMMS
+
+    def __repr__(self) -> str:
+        return ""
+    
+    def add_data(self, data: pd.DataFrame):
+        raise NotImplementedError
+    
     def run(self):
         raise NotImplementedError
