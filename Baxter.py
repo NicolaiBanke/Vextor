b295eb640c1ff64c13335142a6eb59338bd3cdd7
Nicolai Banke <nicolaibanke@hotmail.com>
2025-10-04 21:12:44 +0200

    refactor: the backtest variables are no longer initialized witht the Baxter instance but are set at the start of the .run() method. The Baxter object takes a Strategy and Portfolio parameter as initialization parameters.

--- Baxter.py
+++ Baxter.py
@@ -1,33 +1,64 @@
 import pandas as pd
+import Portfolio
+import Strategy
+
 
 class Baxter(object):
     def __init__(
         self,
-        pnls=[],
-        unrlzd=[],
-        pnls_pct=[],
-        unrlzd_pct=[],
-        trade_dates=[],
-        dates=[],
-        entry=None,
-        inpos=0,
-        COMMS=0.05,
+        strategy: Strategy,
+        portfolio: Portfolio
     ) -> None:
-        self._pnls = pnls
-        self._unrlzd = unrlzd
-        self._pnls_pct = pnls_pct
-        self._unrlzd_pct = unrlzd_pct
-        self._trade_dates = trade_dates
-        self._dates = dates
-        self._entry = entry
-        self._inpos = inpos
-        self._COMMS = COMMS
+        self._strategy = strategy
+        self._portfolio = portfolio
 
-    def __repr__(self) -> str:
-        return ""
-    
-    def add_data(self, data: pd.DataFrame):
-        raise NotImplementedError
-    
-    def run(self):
-        raise NotImplementedError
+    @property
+    def strategy(self):
+        return self._strategy
+
+    @strategy.setter
+    def strategy(self, st: Strategy):
+        self._strategy = st
+
+    @property
+    def portfolio(self):
+        return self._portfolio
+
+    @portfolio.setter
+    def portfolio(self, pf):
+        self._portfolio = pf
+
+    def run(self) -> None:
+        pnls = [],
+        unrlzd = [],
+        pnls_pct = [],
+        unrlzd_pct = [],
+        trade_dates = [],
+        dates = [],
+        entry = None,
+        inpos = 0,
+        COMMS = 0.05,
+
+        for i in range(len(asset)-1):
+            unr = (asset.Close.iloc[i].item() -
+                   asset.Close.iloc[i-1].item()) * inpos
+            unr_pct = ((asset.Close.iloc[i].item(
+            ) - asset.Close.iloc[i-1].item()) / asset.Close.iloc[i-1].item()) * inpos
+            unrlzd.append(unr)
+            unrlzd_pct.append(unr_pct)
+
+            # go long
+            if mr.signal(i, inpos) == "long":
+                entry = asset.Close.iloc[i].item()
+                trade_dates.append(asset.index[i+1])
+                inpos = 1
+
+            # close position
+            elif mr.signal(i, inpos) == "close":
+                pnl = asset.Close.iloc[i].item() - entry
+                pnls.append(pnl)
+                pnl_pct = (asset.Close.iloc[i].item() - entry) / entry
+                pnls_pct.append(pnl_pct)
+                inpos = 0
+            else:
+                pass
