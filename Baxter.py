831eea8ea8175cf4b44e2209a3943329fb24a727
Nicolai Banke <nicolaibanke@hotmail.com>
2025-10-05 22:25:33 +0200

    feat: Baxter.run() method mostly complete and showing plots as well as some clean up
    Baxter object is initialized with portfolio and Strategy

--- Baxter.py
+++ Baxter.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 import Portfolio
 import Strategy
 
@@ -7,58 +9,91 @@ class Baxter(object):
     def __init__(
         self,
         strategy: Strategy,
-        portfolio: Portfolio
+        portfolio: pd.DataFrame
     ) -> None:
-        self._strategy = strategy
-        self._portfolio = portfolio
+        self.strategy = strategy
+        self.portfolio = portfolio
+        self._results = {
+            'pnls': [],
+            'unrlzd': [],
+            'pnls_pct': [],
+            'unrlzd_pct': [],
+            'trade_dates': [],
+            'dates': []
+        }
 
-    @property
-    def strategy(self):
-        return self._strategy
+    def _plot_pnls(self):
+        sns.set_theme()
 
-    @strategy.setter
-    def strategy(self, st: Strategy):
-        self._strategy = st
+        equity_curve_dollar_pnl = pd.Series(self._results['pnls'], index=self._results['trade_dates']).to_frame(
+            'pnls').cumsum()
 
-    @property
-    def portfolio(self):
-        return self._portfolio
+        equity_curve_dollar_unrlzd = pd.Series(
+            self._results['unrlzd'], index=self._results['dates']).to_frame('unrlzd').cumsum()
 
-    @portfolio.setter
-    def portfolio(self, pf):
-        self._portfolio = pf
+        buynhold_dollar = self.portfolio.diff().to_frame("buy and hold").cumsum()
+
+        equity_curve_pct_pnl = (1 + pd.Series(self._results['pnls_pct'], index=self._results['trade_dates'])).to_frame(
+            'pnls_pct').cumprod()
+
+        equity_curve_pct_unrlzd = (
+            1 + pd.Series(self._results['unrlzd_pct'], index=self._results['dates'])).to_frame('unrlzd_pct').cumprod()
+
+        buynhold_unrlzd = (
+            1 + self.portfolio.pct_change().to_frame("buy and hold")).cumprod()
+
+        drawdown_unrlzd = equity_curve_pct_unrlzd / equity_curve_pct_unrlzd.expanding().max() - 1
+        drawdown_pnl = equity_curve_pct_pnl / equity_curve_pct_pnl.expanding().max() - 1
+
+        fig, (ax0, ax1, ax2, ax3) = plt.subplots(
+            4, 1, figsize=(20, 15), sharex=True)
+        fig.suptitle("Equity Curves and Drawdown")
+        ax0.plot(equity_curve_dollar_unrlzd)
+        ax0.plot(buynhold_dollar)
+        ax0.plot(equity_curve_dollar_pnl, 'g+')
+        ax0.legend(['Unrealized', 'Benchmark', 'Realized'])
+        ax0.set_title("Dollar P&L")
+
+        ax1.plot(equity_curve_pct_unrlzd)
+        ax1.plot(buynhold_unrlzd)
+        ax1.plot(equity_curve_pct_pnl, 'g+')
+        ax1.legend(['Unrealized', 'Benchmark', 'Realized'])
+        ax1.set_title("Percentage P&L")
+
+        ax2.plot(drawdown_unrlzd, 'r')
+        ax2.set_title("Unrealized Drawdown")
+        
+        ax3.plot(drawdown_pnl, 'r')
+        ax3.set_title("Realized Drawdown")
 
     def run(self) -> None:
-        pnls = [],
-        unrlzd = [],
-        pnls_pct = [],
-        unrlzd_pct = [],
-        trade_dates = [],
-        dates = [],
-        entry = None,
-        inpos = 0,
-        COMMS = 0.05,
+        entry = None
+        inpos = 0
+        COMMS = 0.05
 
-        for i in range(len(asset)-1):
-            unr = (asset.Close.iloc[i].item() -
-                   asset.Close.iloc[i-1].item()) * inpos
-            unr_pct = ((asset.Close.iloc[i].item(
-            ) - asset.Close.iloc[i-1].item()) / asset.Close.iloc[i-1].item()) * inpos
-            unrlzd.append(unr)
-            unrlzd_pct.append(unr_pct)
+        for i in range(1, len(self.portfolio)-1):
+            unr = (self.portfolio.iloc[i].item() -
+                   self.portfolio.iloc[i-1].item()) * inpos
+            unr_pct = ((self.portfolio.iloc[i].item(
+            ) - self.portfolio.iloc[i-1].item()) / self.portfolio.iloc[i-1].item()) * inpos
+            self._results['unrlzd'].append(unr)
+            self._results['unrlzd_pct'].append(unr_pct)
+            self._results['dates'].append(self.portfolio.index[i])
 
             # go long
-            if mr.signal(i, inpos) == "long":
-                entry = asset.Close.iloc[i].item()
-                trade_dates.append(asset.index[i+1])
+            if self.strategy.signal(i, inpos) == "long":
+                entry = self.portfolio.iloc[i].item()
+                self._results['trade_dates'].append(self.portfolio.index[i+1])
                 inpos = 1
 
             # close position
-            elif mr.signal(i, inpos) == "close":
-                pnl = asset.Close.iloc[i].item() - entry
-                pnls.append(pnl)
-                pnl_pct = (asset.Close.iloc[i].item() - entry) / entry
-                pnls_pct.append(pnl_pct)
+            elif self.strategy.signal(i, inpos) == "close":
+                pnl = self.portfolio.iloc[i].item() - entry
+                self._results['pnls'].append(pnl)
+                pnl_pct = (self.portfolio.iloc[i].item() - entry) / entry
+                self._results['pnls_pct'].append(pnl_pct)
                 inpos = 0
             else:
                 pass
+
+        self._plot_pnls()
